764f4b2c1728eab984b9d50fe722ccb80a2487ad
Moritz Bitsch <moritzbitsch@googlemail.com>
2013-09-05 15:54:24 +0200

    Add simple deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+function die() {
+	echo $1
+	exit 1
+}
+
+function deploy() {
+	test -d "$1" || die "Deploy target is not a directory"
+
+	shopt -s dotglob
+	for f in $1/*
+	do
+		test -e "../${f#$1/}" && rm -i "../${f#$1/}"
+		ln -sf "$(realpath "$f")" ../${f#$1/}
+	done
+}
+
+test -z "$1" && die "Need a project path"
+
+deploy "$1"
